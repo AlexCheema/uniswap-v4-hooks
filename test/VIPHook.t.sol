65ce2942cb1d78da8185730a39dae3f9dd87942b
Alex Cheema <alexcheema123@gmail.com>
2023-07-09 19:18:01 +0100

    test setup with swap and modify position

--- test/VIPHook.t.sol
+++ test/VIPHook.t.sol
@@ -10,6 +10,10 @@ import {PoolSwapTest} from "@uniswap/v4-core/contracts/test/PoolSwapTest.sol";
 import {VIPHook} from "../contracts/VIPHook.sol";
 import {VIPHookImplementation} from "./shared/implementation/VIPHookImplementation.sol";
 import {CurrencyLibrary, Currency} from "@uniswap/v4-core/contracts/libraries/CurrencyLibrary.sol";
+import {TickMath} from "@uniswap/v4-core/contracts/libraries/TickMath.sol";
+import {PoolModifyPositionTest} from "@uniswap/v4-core/contracts/test/PoolModifyPositionTest.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 import {console} from "forge-std/console.sol";
 import {Test} from "forge-std/Test.sol";
@@ -22,9 +26,10 @@ contract TestVIPHook is Test, Deployers {
     TestERC20 token0;
     TestERC20 token1;
     PoolManager manager;
+    PoolModifyPositionTest modifyPositionRouter;
     VIPHook vipHook = VIPHook(address(uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.AFTER_SWAP_FLAG)));
-    IPoolManager.PoolKey key;
-    PoolId id;
+    IPoolManager.PoolKey poolKey;
+    PoolId poolId;
 
     PoolSwapTest swapRouter;
 
@@ -47,21 +52,51 @@ contract TestVIPHook is Test, Deployers {
             }
         }
 
-        key = IPoolManager.PoolKey(Currency.wrap(address(token0)), Currency.wrap(address(token1)), 3000, 60, vipHook);
-        id = key.toId();
-        manager.initialize(key, SQRT_RATIO_1_1);
-
+        modifyPositionRouter = new PoolModifyPositionTest(IPoolManager(address(manager)));
         swapRouter = new PoolSwapTest(manager);
 
-        token0.approve(address(vipHook), type(uint256).max);
-        token1.approve(address(vipHook), type(uint256).max);
-        token0.approve(address(swapRouter), type(uint256).max);
-        token1.approve(address(swapRouter), type(uint256).max);
+        poolKey = IPoolManager.PoolKey(Currency.wrap(address(token0)), Currency.wrap(address(token1)), 3000, 60, vipHook);
+        poolId = poolKey.toId();
+        manager.initialize(poolKey, SQRT_RATIO_1_1);
+
+        /*token0.approve(address(vipHook), type(uint256).max);
+        token1.approve(address(vipHook), type(uint256).max);*/
+
+        token0.approve(address(modifyPositionRouter), 100 ether);
+        token1.approve(address(modifyPositionRouter), 100 ether);
+        token0.approve(address(swapRouter), 100 ether);
+        token1.approve(address(swapRouter), 100 ether);
+        token0.mint(address(this), 100 ether);
+        token1.mint(address(this), 100 ether);
+        /*modifyPositionRouter.modifyPosition(poolKey, IPoolManager.ModifyPositionParams(-60, 60, 10 ether));
+        modifyPositionRouter.modifyPosition(poolKey, IPoolManager.ModifyPositionParams(-120, 120, 10 ether));
+        modifyPositionRouter.modifyPosition(
+            poolKey, IPoolManager.ModifyPositionParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 10 ether)
+        );*/
     }
 
 
     function testPlaceholder() public {
         console.log("testPlaceholder");
+        console.log(token0.balanceOf(address(vipHook)));
+        console.log(token0.balanceOf(address(swapRouter)));
+        console.log(token0.balanceOf(address(this)));
+
+        int24 tickLower = 0;
+        bool zeroForOne = true;
+        uint128 liquidity = 1000000;
+        // vipHook.place(poolKey, tickLower, zeroForOne, liquidity);
+
+        modifyPositionRouter.modifyPosition(poolKey, IPoolManager.ModifyPositionParams(-60, 60, 10 ether));
+
+        BalanceDelta balanceDelta = swapRouter.swap(
+            poolKey,
+            IPoolManager.SwapParams(false, 1 ether, TickMath.getSqrtRatioAtTick(60)),
+            PoolSwapTest.TestSettings(true, true)
+        );
+
+        (, int24 tick,,,,) = manager.getSlot0(poolId);
+        assertEq(tick, 60);
     }
 
 }
